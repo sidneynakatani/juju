1c5bbec54e2b99961f960086d83adee9684eb194
sidney <sidney.nakatani@hotmail.com>
2015-03-29 23:21:19 -0300

    remove method

--- static/js/mapUtil2.js
+++ static/js/mapUtil2.js
@@ -31,56 +31,6 @@ function setMarkers(map, locations, list) {
 
 
 
-function createTable(list, map){
-				var footer = document.getElementById("footer");
-				var divMain = document.createElement('div');
-				divMain.className = "owl-carousel";
-				
-				
-        			for (var j = 0; j < list.length; j++) {
-						        
-					//cell.addEventListener("click", function() {
-   					//				focus(this.id, true, map);
-					//				});
-
-					
-					var divItem = document.createElement('div');
-					divItem.className = "item";
-
-                			var cellText = document.createTextNode(list[j].title);
-                                        var img = document.createElement('img');
-                                        img.src = "/static/img/cat.png";
-					//img.src = "http://res.cloudinary.com/nakatani/image/upload/v1419728610/q6ostz0ydvkdh1hk0xzc.png";
-                                        img.setAttribute('width', '25%');
-					img.setAttribute('height', '30%');
-
-										
-
-					var divImg = document.createElement('div');
-					divImg.appendChild(img);
-
-					var divText = document.createElement('div');
-					divText.appendChild(cellText);
-
-					divItem.appendChild(divImg);
-					divItem.appendChild(divText);
-
-					//var h = document.createElement("H1")            
-					//var t = document.createTextNode(j);
-					//h.appendChild(t);
-					//divItem.appendChild(h);
-
-					divMain.appendChild(divItem);
-
-        
-        			}
-			
-				
-        			footer.appendChild(divMain);
-        			
-			}
-
-
 function focus(position, isVisible, map){
                              
                             if(isVisible == true){
@@ -124,6 +74,7 @@ function customDataSuccess(data){
                       div.appendChild(divText);
                       div.appendChild(divImg);
 		      div.setAttribute("align", "center");
+		      div.setAttribute("style", "border:3px solid black; margin-left: 50px; margin-right: 50px;");
 		      div.setAttribute("id", i);
 		      div.addEventListener("click", function() {
                                                 focus(this.id, true, map);
